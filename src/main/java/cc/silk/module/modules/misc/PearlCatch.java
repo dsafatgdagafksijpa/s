ca652e921bb3dc7046b30181a7e844c8cdec2d29
Vquxla <yoblizzified@gmail.com>
2026-02-17 14:10:16 -0500

    Update PearlCatch.java
    fixed switchback

--- src/main/java/cc/silk/module/modules/misc/PearlCatch.java
+++ src/main/java/cc/silk/module/modules/misc/PearlCatch.java
@@ -10,25 +10,27 @@ import cc.silk.utils.keybinding.KeyUtils;
 import cc.silk.utils.math.TimerUtil;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.item.Items;
-import net.minecraft.util.Hand;
 import org.lwjgl.glfw.GLFW;
 
 public final class PearlCatch extends Module {
-    private final KeybindSetting pearlChargeKeybind = new KeybindSetting("Pearl Charge Key", GLFW.GLFW_KEY_H, true);
-    private final NumberSetting windDelay = new NumberSetting("Wind Delay", 0, 2000, 200, 1);
-    private final NumberSetting switchDelay = new NumberSetting("Switch Delay", 0, 500, 50, 10);
+
+    private final KeybindSetting pearlChargeKeybind =
+            new KeybindSetting("Pearl Charge Key", GLFW.GLFW_KEY_H, true);
+
+    private final NumberSetting windDelay =
+            new NumberSetting("Wind Delay", 0, 2000, 200, 1);
 
     private final TimerUtil pearlDelayTimer = new TimerUtil();
-    private final TimerUtil switchTimer = new TimerUtil();
+
     private boolean keyPressed = false;
     private boolean pearlThrown = false;
     private int originalSlot = -1;
-    private boolean needsSwitchBack = false;
 
     public PearlCatch() {
         super("Pearl Catch", "Throws pearl then windcharge", -1, Category.MISC);
-        this.addSettings(pearlChargeKeybind, windDelay, switchDelay);
-        this.getSettings().removeIf(setting -> setting instanceof KeybindSetting && !setting.equals(pearlChargeKeybind));
+        this.addSettings(pearlChargeKeybind, windDelay);
+        this.getSettings().removeIf(setting ->
+                setting instanceof KeybindSetting && !setting.equals(pearlChargeKeybind));
     }
 
     @EventHandler
@@ -46,12 +48,6 @@ public final class PearlCatch extends Module {
             pearlThrown = false;
         }
 
-        if (needsSwitchBack && switchTimer.hasElapsedTime(switchDelay.getValueInt())) {
-            mc.player.getInventory().selectedSlot = originalSlot;
-            needsSwitchBack = false;
-            originalSlot = -1;
-        }
-
         keyPressed = currentKeyState;
     }
 
@@ -59,15 +55,12 @@ public final class PearlCatch extends Module {
         int pearlSlot = findPearlSlot();
         if (pearlSlot == -1) return;
 
-        if (mc.player.getItemCooldownManager().isCoolingDown(new net.minecraft.item.ItemStack(Items.ENDER_PEARL))) {
+        if (mc.player.getItemCooldownManager().isCoolingDown(Items.ENDER_PEARL)) {
             return;
         }
 
-        originalSlot = mc.player.getInventory().selectedSlot;
         mc.player.getInventory().selectedSlot = pearlSlot;
         ((MinecraftClientAccessor) mc).invokeDoItemUse();
-        needsSwitchBack = true;
-        switchTimer.reset();
 
         pearlThrown = true;
         pearlDelayTimer.reset();
@@ -77,15 +70,12 @@ public final class PearlCatch extends Module {
         int windChargeSlot = findWindChargeSlot();
         if (windChargeSlot == -1) return;
 
-        if (mc.player.getItemCooldownManager().isCoolingDown(new net.minecraft.item.ItemStack(Items.WIND_CHARGE))) {
+        if (mc.player.getItemCooldownManager().isCoolingDown(Items.WIND_CHARGE)) {
             return;
         }
 
-        originalSlot = mc.player.getInventory().selectedSlot;
         mc.player.getInventory().selectedSlot = windChargeSlot;
         ((MinecraftClientAccessor) mc).invokeDoItemUse();
-        needsSwitchBack = true;
-        switchTimer.reset();
     }
 
     private int findPearlSlot() {
@@ -111,9 +101,7 @@ public final class PearlCatch extends Module {
         keyPressed = false;
         pearlThrown = false;
         originalSlot = -1;
-        needsSwitchBack = false;
         pearlDelayTimer.reset();
-        switchTimer.reset();
         super.onEnable();
     }
 
